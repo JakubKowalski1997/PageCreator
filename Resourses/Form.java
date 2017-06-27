ef4653ca18e381107bccfce3e33a314b7a930a79
Wiktor Łazarski <wlazarski@interia.pl>
2017-06-27 22:35:12 +0200

    First step - create data structure

--- Resourses/Form.java
+++ Resourses/Form.java
@@ -1,8 +1,30 @@
+import java.util.*;
+
 /**
  * Created by Wiktor Łazarski on 27.06.2017.
  */
 public class Form {
-    public static void main(String[] args){
-        System.out.println("Hello PageCreator");
+    //Enumeration list of attributes used to have easy access to Map of Form's data
+    private enum HeadAttrib{
+        NAME,
+        SURNAME,
+        TITLE,
+        KEYWORDS,
+        DESCRIPTION,
+        CHARSET
+    }
+
+    //Map for all the Form's data
+    private Map<HeadAttrib, String> formData;
+
+    //constructor
+    public Form(/*FormWindow with textfields will be argument*/)
+    {
+        //init Map
+        formData = new HashMap<>();
+
+        //simple fast test if everything is correnct
+        formData.put(HeadAttrib.CHARSET, "SDSD");
+        System.out.println(formData);
     }
 }
