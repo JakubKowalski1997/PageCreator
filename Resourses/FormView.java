ce6e839ec976fb9c262b9a586ed4b1efb4c98ded
Wiktor Łazarski <wlazarski@interia.pl>
2017-07-02 22:26:20 +0200

    Create array for textfields

--- Resourses/FormView.java
+++ Resourses/FormView.java
@@ -7,22 +7,31 @@ import java.util.ArrayList;
  */
 public class FormView extends JFrame {
 
-    //textareadimensions
+    //textarea dimensions
     private static final int TEXTAREA_ROWS = 8;
     private static final int TEXTAREA_COLUMNS = 20;
 
     //queries used in JLabels
     private ArrayList<String> queries;
 
+    //textareas
+    private ArrayList<JTextField> textFields;
+
     private void createAndAddComponents(ArrayList<String> queries){
         JPanel centerPanel = new JPanel();
         centerPanel.setBackground(new Color(238, 163, 65));
         centerPanel.setLayout(new GridLayout(6, 2));
 
+        //init textarea's array
+        textFields = new ArrayList<>();
+
         //create components loop
         for(int i = 0; i < 6; i++){
             centerPanel.add(new JLabel(queries.get(i), JLabel.LEFT));
-            centerPanel.add(new JTextField());
+
+            //add TextField to ArrayList and centerPanel
+            textFields.add(new JTextField());
+            centerPanel.add(textFields.get(i));
         }
 
         add(centerPanel, BorderLayout.CENTER);
@@ -48,7 +57,7 @@ public class FormView extends JFrame {
     //Window constructor
     FormView(){
         //create ArrayList
-        queries = new ArrayList<>();
+        queries = new ArrayList<>(6);
         //filling queries ArrayList
         queries.add("Authors name : ");
         queries.add("Authors lastname : ");
