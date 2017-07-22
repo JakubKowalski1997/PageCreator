2975ab84e903a128a8914ae5275a682a4c662af7
Wiktor Łazarski <wlazarski@interia.pl>
2017-07-22 13:14:34 +0200

    Fixed FONT bug

--- Resources/MainEditor/MVCTitle/TitleController.java
+++ Resources/MainEditor/MVCTitle/TitleController.java
@@ -12,7 +12,7 @@ public class TitleController {
 
     public TitleController(TitleView view){
         HTMLTitlePreviewPanel currVals = view.returnVisualizingPanel();
-        Font currFont = currVals.getFont();
+        Font currFont = currVals.getTextField().getFont();
 
         //creating model based on View
         model = new TitleModel();
@@ -27,6 +27,7 @@ public class TitleController {
             case JTextField.RIGHT :
                 model.setPosition("right");break;
         }
+        System.out.println(model.getFont().toString());
     }
 
     //return created model
